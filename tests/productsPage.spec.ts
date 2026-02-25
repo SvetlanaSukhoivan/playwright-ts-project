7a5a055cc60a9be0b18d605fce937487c5f623ea
Svetlana Sukhoivan lvrx <svetlana.sukhoivanlvrx@gmail.com>
2026-02-25 11:44:39 +0300

    try to push last changes to gitlab

--- tests/productsPage.spec.ts
+++ tests/productsPage.spec.ts
@@ -18,6 +18,6 @@ test.describe("products page test", () => {
         const data = await productPage.getCategoriesText();
 
         expect(data).toEqual(arrCategories);
+        
     });
 });   
-

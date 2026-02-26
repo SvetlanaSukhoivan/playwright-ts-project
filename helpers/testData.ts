e168101665e908236b03bde963cd94e63cec72d3
Svetlana Sukhoivan lvrx <svetlana.sukhoivanlvrx@gmail.com>
2026-02-26 11:17:58 +0300

    delete comments

--- helpers/testData.ts
+++ helpers/testData.ts
@@ -1,19 +1,8 @@
-// export const contactUsData = {
-//     name: 'Lenny',
-//     email: 'lennytest@myemail.com',
-//     message: 'my first message',
-//     successSubmissionMessage: 'Success! Your details have been submitted successfully.'
-// }
-
-// export const arrCategories = ['WOMEN', 'MEN', 'KIDS'];
-
 export const contactUsData = {
     name: 'my name',
     email: 'myemai@myemail.com',
     message: 'my message',
     successSubmissionMessage: 'Success! Your details have been submitted successfully.'
 }
-
-
 
 export const arrCategories = ['WOMEN', 'MEN', 'KIDS'];
--- pages/basePage.ts
+++ pages/basePage.ts
@@ -2,16 +2,13 @@ import { Page, Locator } from "@playwright/test";
 
 export default class BasePage {
     protected page: Page;
-// Сохраняем локатор как свойство класса, т.к. он часто нужен
+
     protected consentButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.consentButton = this.page.getByRole('button',{ name: 'Consent', exact: true });
-    
-    // МАГИЯ PLAYWRIGHT: Регистрируем обработчик один раз в конструкторе
-    // Как только этот элемент появится в DOM и станет видимым,
-    // Playwright приостановит тест, выполнит этот код, и продолжит тест.
+   
         this.page.addLocatorHandler(
             this.consentButton,
             async () => {
@@ -25,29 +22,3 @@ export default class BasePage {
         await this.page.goto(url);
     }
 }
-
-
-// export default class BasePage {
-//     protected page: Page;
-
-//     constructor(page: Page) {
-//         this.page = page;
-//     }
-
-//     async navigate(url: string) {
-//         await this.page.goto(url);
-//         await this.handleConsentPopup();
-//     }
-
-//     async handleConsentPopup() {
-//         // button Locator
-//         const consentButton = this.page.getByRole('button', { name: 'Consent', exact: true });
-
-//         // Короткий таймаут, чтобы не ждать долго, если поп-апа нет
-//         if (await consentButton.isVisible({ timeout: 5000 })) {
-//             await consentButton.click();
-//             //ожидание исчезновения поп-апа
-//             await consentButton.waitFor({ state: "hidden" });
-//         }
-//     }
-// }

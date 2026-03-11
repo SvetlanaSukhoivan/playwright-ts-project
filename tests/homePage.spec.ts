1173baf6790a7cc718a4e880ef0aa8226c6125d7
Svetlana Sukhoivan <svetlana.sukhoivan.mackbook@examplemail.com>
2026-03-11 11:28:06 +0200

    build: setup ESLint, Husky and fix test code style

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,19 +1,17 @@
-import { test , expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import Header from '../pages/header';
 import HomePage from '../pages/homePage';
 
-
 test.describe('homepage test', () => {
-    test.beforeEach(async ({ page }) => {
-        const homePage = new HomePage(page);
-        await homePage.loadHomePage();
-    });
+  test.beforeEach(async ({ page }) => {
+    const homePage = new HomePage(page);
+    await homePage.loadHomePage();
+  });
 
-    test("verify home page", async ({ page }) => {  
-        const header = new Header(page);
+  test('verify home page', async ({ page }) => {
+    const header = new Header(page);
 
-        const loc = header.locators.getHomePageLink();
-        await expect(loc).toHaveCSS("color", "rgb(255, 165, 0)");
-
-    });
-});    
+    const loc = header.locators.getHomePageLink();
+    await expect(loc).toHaveCSS('color', 'rgb(255, 165, 0)');
+  });
+});
